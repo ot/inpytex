87f8f8bcaeda0154e1bd3d89a90c9e36d0a31772
Giuseppe Ottaviano <giuott@gmail.com>
2012-09-10 14:08:38 +0200

    Better error reporting, fix bug in setup.py, explain labels in README

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ if __name__ == '__main__':
     from distutils.core import setup
     setup(name='inpytex',
           version='0.1',
-          scripts=['inpytex.py']
+          scripts=['inpytex']
           )
